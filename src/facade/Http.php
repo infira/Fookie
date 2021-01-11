04c1353acbb879e2a6a5e63bd83fdc4ce6145869
Gen Taliaru <gen@infira.ee>
2021-01-11 10:51:51 +0200

    must check isset

--- src/facade/Http.php
+++ src/facade/Http.php
@@ -5,6 +5,10 @@ class Http extends \Infira\Utils\Http
 {
 	public static function acceptJSON(): bool
 	{
+		if (!isset($_SERVER['HTTP_ACCEPT']))
+		{
+			return false;
+		}
 		if (strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false)
 		{
 			return true;
