90ccc86670feeb3720a8829fcd846c26fd4751c5
Gen Taliar <gen@infira.ee>
2021-05-24 14:23:19 +0300

    code cleanup

--- src/PathHandler.php
+++ src/PathHandler.php
@@ -2,26 +2,24 @@
 
 namespace Infira\Fookie;
 
-use Path;
-
 abstract class PathHandler
 {
-	public static final function root(string $path = "", $getAsUrl = false)
+	public static final function root(string $path = "", $getAsUrl = false): string
 	{
 		return self::base(BASE_DIR, $path, $getAsUrl);
 	}
 	
-	public static final function app(string $path = "", $getAsUrl = false)
+	public static function app(string $path = "", $getAsUrl = false): string
 	{
 		return self::base(APP_DIR, $path, $getAsUrl);
 	}
 	
-	public static final function temp($file = "", $getAsUrl = false)
+	public static function temp(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::base(TEMP_DIR, $file, $getAsUrl);
 	}
 	
-	private static final function base(string $base, string $path = "", $getAsUrl = false)
+	private static function base(string $base, string $path = "", $getAsUrl = false): string
 	{
 		if (is_dir($path))
 		{
@@ -44,27 +42,27 @@ abstract class PathHandler
 		}
 	}
 	
-	public static final function urlPseudo($string)
+	public static function urlPseudo(string $string): string
 	{
 		return str_replace(self::root(false, true), "[BASE_URL]", $string);
 	}
 	
-	public static final function pseudoUrl($string)
+	public static function pseudoUrl(string $string): string
 	{
 		return str_replace("[BASE_URL]", self::root(false, true), $string);
 	}
 	
-	public static final function pseudoPath($string)
+	public static function pseudoPath(string $string): string
 	{
 		return str_replace(["[BASE_URL]", "%5BBASE_URL%5D"], self::root(), $string);
 	}
 	
-	public static final function toPath($path)
+	public static function toPath(string $path): string
 	{
 		return str_replace(SITE_URL, BASE_DIR, $path);
 	}
 	
-	public static final function toUrl($path)
+	public static function toUrl(string $path): string
 	{
 		$dir = str_replace(DIRECTORY_SEPARATOR, "/", $path);
 		$dir = str_replace(BASE_DIR, SITE_URL, $dir);
@@ -73,7 +71,7 @@ abstract class PathHandler
 		return SITE_URL . $dir;
 	}
 	
-	public static final function fix($dir)
+	public static final function fix(string $dir): string
 	{
 		if ($dir) //if empty reutrn empty
 		{
@@ -111,47 +109,47 @@ abstract class PathHandler
 	
 	//################################################################### SOF APP paths
 	
-	public static final function assets($file = "", $getAsUrl = false)
+	public static function assets(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::app("assets/" . $file, $getAsUrl);
 	}
 	
-	public static final function controller($file = "", $getAsUrl = false)
+	public static function controller(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::app("controller/" . $file, $getAsUrl);
 	}
 	
-	public static final function facade($file = "", $getAsUrl = false)
+	public static function facade(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::app("facade/" . $file, $getAsUrl);
 	}
 	
-	public static final function helper($file = "", $getAsUrl = false)
+	public static function helper(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::app("helper/" . $file, $getAsUrl);
 	}
 	
-	public static final function model($file = "", $getAsUrl = false)
+	public static function model(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::app("model/" . $file, $getAsUrl);
 	}
 	
-	public static final function modelModels($file = "", $getAsUrl = false)
+	public static function modelModels(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::model("models/" . $file, $getAsUrl);
 	}
 	
-	public static final function modelExtensions($file = "", $getAsUrl = false)
+	public static function modelExtensions(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::model("extensions/" . $file, $getAsUrl);
 	}
 	
-	public static final function service($file = "", $getAsUrl = false)
+	public static function service(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::app("service/" . $file, $getAsUrl);
 	}
 	
-	public static final function view($file = "", $getAsUrl = false)
+	public static function view(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::app("view/" . $file, $getAsUrl);
 	}
@@ -159,37 +157,37 @@ abstract class PathHandler
 	//################################################################### EOF APP paths
 	
 	//################################################################### SOF Fookie paths
-	public static final function fookie($file = "", $getAsUrl = false)
+	public static function fookie(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::root("vendor/infira/fookie/src/" . $file, $getAsUrl);
 	}
 	
-	public static final function fookieJS($file = "", $getAsUrl = false)
+	public static function fookieJS(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::fookie("js/" . $file, $getAsUrl);
 	}
 	
-	public static final function fookieTraits($file = "", $getAsUrl = false)
+	public static function fookieTraits(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::fookie("traits/" . $file, $getAsUrl);
 	}
 	
-	public static function db(string $file = "", bool $getAsUrl = false)
+	public static function db(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::root("db/" . $file, $getAsUrl);
 	}
 	
-	public static function dbViews(string $file = "", bool $getAsUrl = false)
+	public static function dbViews(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::db("views/" . $file, $getAsUrl);
 	}
 	
-	public static function dbTriggers(string $file = "", bool $getAsUrl = false)
+	public static function dbTriggers(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::db("triggers/" . $file, $getAsUrl);
 	}
 	
-	public static function systemUpdates(string $file = "", bool $getAsUrl = false)
+	public static function systemUpdates(string $file = "", bool $getAsUrl = false): string
 	{
 		return self::db("updates/" . $file, $getAsUrl);
 	}
